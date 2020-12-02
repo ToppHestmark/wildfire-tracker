dcb28720dce68a6139e2519feed801e32d89498e
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-12-02 22:48:13 +0100

    rendered geometries from API to the map

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,7 +2,13 @@ import GoogleMapReact from 'google-map-react';
 import { config } from './../config';
 import LocationMarker from './LocationMarker';
 
-const Map = ({ center, zoom }) => {
+const Map = ({ eventData, center, zoom }) => {
+  const markers = eventData.map(ev => {
+    return ev.categories[0].id === 8
+      ? <LocationMarker key={ev.id} lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} />
+      : null;
+  })
+
   return (
     <div className="map">
       <GoogleMapReact
@@ -10,7 +16,7 @@ const Map = ({ center, zoom }) => {
           defaultCenter={center}
           defaultZoom={zoom}
         >
-          <LocationMarker lat={center.lat} lng={center.lng} />
+          {markers}
       </GoogleMapReact>
     </div>
   )
@@ -21,7 +27,7 @@ Map.defaultProps = {
     lat: 36.778259,
     lng: -119.417931
   },
-  zoom: 6
+  zoom: 5
 }
 
-export default Map
+export default Map;
