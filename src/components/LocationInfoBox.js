febcd674f4c09e5022a717f8cf04c4c2bd4c863d
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-12-02 23:49:26 +0100

    Completed with header and infobox

---
+++ src/components/LocationInfoBox.js
@@ -0,0 +1,15 @@
+import './LocationInfoBox.css';
+
+const LocationInfoBox = ({ info }) => {
+  return (
+    <div className="locationInfoBox">
+      <h2>Event Location Info</h2>
+      <ul>
+        <li>ID: <strong> {info.id} </strong></li>
+        <li>TITLE: <strong> {info.title} </strong></li>
+      </ul>
+    </div>
+  )
+}
+
+export default LocationInfoBox;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 import { config } from './../config';
 import LocationMarker from './LocationMarker';
+import LocationInfoBox from './LocationInfoBox';
 
 const Map = ({ eventData, center, zoom }) => {
+  const [locationInfo, setLocationInfo] = useState(null);
+
   const markers = eventData.map(ev => {
     return ev.categories[0].id === 8
-      ? <LocationMarker key={ev.id} lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} />
+      ? <LocationMarker 
+        onClick={() => setLocationInfo({ id: ev.id, title: ev.title })}
+        key={ev.id}  
+        lat={ev.geometries[0].coordinates[1]} 
+        lng={ev.geometries[0].coordinates[0]} 
+        />
       : null;
   })
 
@@ -18,14 +27,15 @@ const Map = ({ eventData, center, zoom }) => {
         >
           {markers}
       </GoogleMapReact>
+      {locationInfo && <LocationInfoBox info={locationInfo} />}
     </div>
   )
 }
 
 Map.defaultProps = {
   center: {
-    lat: 36.778259,
-    lng: -119.417931
+    lat: 39.550053,
+    lng: -105.782066
   },
   zoom: 5
 }
